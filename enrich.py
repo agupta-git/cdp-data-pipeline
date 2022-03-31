6cd8a43a0e42617ef774b18fae5fb9b3d5530a9f
Anshul Gupta <aguptapro@gmail.com>
2022-03-31 10:04:05 -0700

    Added assets

--- enrich.py
+++ enrich.py
@@ -7,51 +7,51 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import pyspark.sql.functions as F
 
-## CREATE SPARK SESSION
+## Create Spark session
 spark = SparkSession.builder.appName('Enrich').getOrCreate()
 spark.conf.set("spark.sql.legacy.allowCreatingManagedTableUsingNonemptyLocation", "true")
-print("JOB STARTED")
+print("Job started...")
 
-## LOAD DATA FROM AWS S3 CLOUD STORAGE
-fs = "s3a://pse-uat2/angupta-demo"
+## Load data from AWS S3 cloud storage
+fs = "s3a://pse-uat2/angupta-demo" # UPDATE THIS VARIABLE TO POINT TO YOUR BUCKET
 covid_rate_by_soc_det = spark.read.csv(fs + "/covid-19-case-rate-by-social-det.csv", header=True, inferSchema=True)
 covid_demo_rate_cumulative = spark.read.csv(fs + "/covid-19-demographic-rate-cumulative.csv", header=True, inferSchema=True)
 cdph_data_dictionary = spark.read.csv(fs + "/covid-19-equity-metrics-data-dictionary.csv", header=True, inferSchema=True)
 members_profile = spark.read.csv(fs + "/member_profile.csv", header=True, inferSchema=True)
 
-## CLEANUP DATABASES
-print("DATABASE CLEANUP STARTED...")
+## Cleanup databases
+print("Database cleanup started...")
 spark.sql("DROP DATABASE IF EXISTS CDPH CASCADE")
 spark.sql("DROP DATABASE IF EXISTS MEMBER CASCADE")
-print("DATABASE CLEANUP COMPLETED")
+print("Database cleanup completed")
 
-## CREATE DATABASES
-print("CREATE DATABASE STARTED...")
+## Create databases
+print("Create database started...")
 spark.sql("CREATE DATABASE CDPH")
 spark.sql("CREATE DATABASE MEMBER")
-print("CREATE DATABASE COMPLETED")
+print("Create database completed")
 
-## POPULATE RAW DATA
-print("POPULATE RAW DATA STARTED...")
+## Populate raw data
+print("Populate raw data started...")
 
-print("LOADING CDPH.COVID_RATE_BY_SOC_DET...")
+print("Load CDPH.COVID_RATE_BY_SOC_DET...")
 # rename columns that conflict with Hive keywords
 covid_rate_by_soc_det = covid_rate_by_soc_det.withColumnRenamed('date', 'load_date')
 covid_rate_by_soc_det = covid_rate_by_soc_det.withColumnRenamed('sort', 'priority_sort')
 covid_rate_by_soc_det.write.mode("overwrite").saveAsTable('CDPH.COVID_RATE_BY_SOC_DET', format="parquet")
 
-print("LOADING CDPH.COVID_DEMO_RATE_CUMULATIVE...")
+print("Load CDPH.COVID_DEMO_RATE_CUMULATIVE...")
 covid_demo_rate_cumulative.write.mode("overwrite").saveAsTable('CDPH.COVID_DEMO_RATE_CUMULATIVE', format="parquet")
 
-print("LOADING CDPH.DATA_DICTIONARY...")
+print("Load CDPH.DATA_DICTIONARY...")
 cdph_data_dictionary.write.mode("overwrite").saveAsTable('CDPH.DATA_DICTIONARY', format="parquet")
 
-print("LOADING MEMBER.MEMBER_PROFILE...")
+print("Load MEMBER.MEMBER_PROFILE...")
 members_profile.write.mode("overwrite").saveAsTable('MEMBER.MEMBER_PROFILE', format="parquet")
-print("POPULATE RAW DATA COMPLETED")
+print("Populate raw data completed")
 
-## POPULATE ENRICHED DATA
-print("POPULATE ENRICHED DATA STARTED...")
+## Populate enriched data
+print("Populate enriched data started...")
 # classify members based on age social factor. This is to be later used in targeting members whose age group is signifantly impacted by covid-19.
 sql_target_members_by_age_group = "with cte_covid_demo_rate_cumulative_upd as ( \
   select *, \
@@ -74,7 +74,7 @@ select distinct c.name, c.email, c.age, b.demographic_set_category \
 from cte_covid_demo_rate_cumulative_upd b \
 join member.member_profile c on c.age between b.low_point and b.high_point"
 table_target_members_by_age_group = spark.sql(sql_target_members_by_age_group)
-print("LOADING MEMBER.TARGET_MBRS_BY_AGE_GROUP...")
+print("Load MEMBER.TARGET_MBRS_BY_AGE_GROUP...")
 table_target_members_by_age_group.write.mode("overwrite").saveAsTable('MEMBER.TARGET_MBRS_BY_AGE_GROUP', format="parquet")
 
 # classify members based on income social factor. This is to be later used in targeting members whose income group is signifantly impacted by covid-19.
@@ -103,8 +103,8 @@ select distinct c.name, c.email, c.income, b.social_tier \
 from cte_covid_rate_by_soc_det_upd b \
 join member.member_profile c on c.income between b.low_point and b.high_point"
 table_target_members_by_income = spark.sql(sql_target_members_by_income)
-print("LOADING MEMBER.TARGET_MBRS_BY_INCOME...")
+print("Load MEMBER.TARGET_MBRS_BY_INCOME...")
 table_target_members_by_income.write.mode("overwrite").saveAsTable('MEMBER.TARGET_MBRS_BY_INCOME', format="parquet")
-print("POPULATE ENRICHED DATA COMPLETED")
+print("Populate enriched data completed")
 
-print("JOB COMPLETED")
+print("Job completed")
